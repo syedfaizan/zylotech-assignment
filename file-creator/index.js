afeb1dc9a963cdd36b5ebc6ae4b6c50e370436d3
Syed Faizan <syedfaizan2008@gmail.com>
2018-12-13 11:54:02 +0530

    problem 1 - file creator

--- file-creator/index.js
+++ file-creator/index.js
@@ -1,19 +1,55 @@
-const inquirer = require('inquirer');
+var readline = require('readline');
+var fs = require('fs');
+var _ = require('lodash');
+const EOL = require('os').EOL;
 
-var questions = [{
-  type: 'input',
-  name: 'No. of files',
-  message: "",
-}, {
-  type: 'input',
-  name: 'sequence',
-  message: "",
-}]
+var input = [];
 
-inquirer
-  .prompt(questions)
-  .then(res => {
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+rl.prompt();
 
+rl.on('line', function (cmd) {
 
-  })
+    input.push(cmd);
+});
+
+var getApppendedFileName = function( fileNames ){
+  fileName
+} 
+
+rl.on('close', function (cmd) {
+    let fileDetails = [];
+    var numberOfFiles = input.shift();
+    var newInput  = input.join(EOL)
+                        .replace(/\$/g, '') // Remove all '$' symbols
+                        .replace(/\n/g, '') // Remove all '\n' or new line characters
+                        .split('\r')
+                        .filter((v,i,a) => a.indexOf(v) == i)
+                        .join('\r')
+                        .split('\r\r')
+                        .map( item => item.split('\r'));
+
+    // making a meaningful JSON object of the date provided
+    fileDetails = newInput.map( item => {
+      var obj = {};
+      obj['fileName'] = item[0];
+      obj['fileContent'] = item.splice(1);
+      return obj;
+    })
+
+    //isolating and concatenating fileNames
+    var fileNames = _.map(fileDetails, 'fileName');
+    var fileExtension = fileNames[0].split('.')[1];
+
+    var appendedFileName = _.camelCase(fileNames.map( (item) => item.split('.')[0] )) + '.' + fileExtension;
+
+    // writing file to same folder with expected output!
+    fs.writeFileSync(appendedFileName, appendedFileName +'\n' +_.flatten(_.map(fileDetails, 'fileContent')).join('\n'), 'utf8')
+
+    process.exit(0);
+  
+});
---
+++ file-creator/readme.md
@@ -0,0 +1,6 @@
+## Instructions
+
+- cd to project directory (file-creator)
+- Run using 'node ./index.js'
+- enter Input (check samepleInput.text)
+- Press `Ctrl + C` to end Input and see output.
